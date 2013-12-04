67928da75f0b654343f33be2ae139bd1519d4ce6
Jin Budelmann <jin@vendhq.com>
2013-12-05 10:48:35 +1300

    Split js into separate file

---
+++ talk.js
@@ -0,0 +1,50 @@
+function talk() {
+  var participants = gapi.hangout.getParticipants();
+
+  for (var index in participants) {
+    var participant = participants[index];
+
+    if (participant.id !== gapi.hangout.getLocalParticipantId()) {
+      gapi.hangout.av.muteParticipantMicrophone(participant.id);
+    } else {
+      activeParticipant = participant;
+      document.getElementById('talker-image').src = activeParticipant.person.image.url;
+    }
+  }
+  gapi.hangout.av.setMicrophoneMute(false);
+}
+
+function unmute() {
+  gapi.hangout.data.setValue("muted", false);
+}
+
+function onStateChange(stateChangeEvent) {
+  var muted = gapi.hangout.data.getValue("muted");
+  gapi.hangout.av.setMicrophoneMute(muted);
+}
+
+function configureSession() {
+  var muted = gapi.hangout.data.getValue("muted");
+  if (typeof muted === 'undefined') {
+    gapi.hangout.data.setValue("muted", true);
+  } else {
+    gapi.hangout.av.setMicrophoneMute(muted);
+  }
+}
+
+// This runs when the gadget is ready
+function init() {
+  // When API is ready...
+  gapi.hangout.onApiReady.add(
+    function(eventObj) {
+      if (eventObj.isApiReady) {
+        configureSession();
+
+        gapi.hangout.data.onStateChanged.add(onStateChange);
+      }
+    }
+  );
+}
+ 
+// Wait for gadget to load.
+gadgets.util.registerOnLoadHandler(init);
