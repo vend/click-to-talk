1d31e446cb785aeac8b4f2347744d692006850ee
Jin Budelmann <jin@vendhq.com>
2013-12-06 08:49:01 +1300

    Store talking participant in state and use to check if mute and populate avatar

--- talk.js
+++ talk.js
@@ -1,5 +1,6 @@
 function talk() {
   var participants = gapi.hangout.getParticipants();
+  gapi.hangout.data.setValue("talking", gapi.hangout.getLocalParticipantId());
   gapi.hangout.data.setValue("muted", "true");
 
   for (var index in participants) {
@@ -7,21 +8,37 @@ function talk() {
 
     if (participant.id !== gapi.hangout.getLocalParticipantId()) {
       gapi.hangout.av.muteParticipantMicrophone(participant.id);
-    } else {
-      activeParticipant = participant;
-      document.getElementById('talker-image').src = activeParticipant.person.image.url;
     }
   }
-  gapi.hangout.av.setMicrophoneMute(false);
 }
 
 function unmute() {
   gapi.hangout.data.setValue("muted", "false");
+  gapi.hangout.data.setValue("talking", "");
 }
 
 function onStateChange(stateChangeEvent) {
   var muted = gapi.hangout.data.getValue("muted");
-  gapi.hangout.av.setMicrophoneMute(muted === "true");
+  if (typeof muted !== 'undefined') {
+    if (muted === "true") {
+        var activeParticipantID = gapi.hangout.data.getValue("talking");
+        var activeParticipant = gapi.hangout.getParticipantById(activeParticipantID);
+
+        if (activeParticipantID === gapi.hangout.getLocalParticipantId()) {
+          gapi.hangout.av.setMicrophoneMute(false);
+        } else {
+          gapi.hangout.av.setMicrophoneMute(true);
+        };
+
+        if (activeParticipant) {
+          document.getElementById('talker-image').src = activeParticipant.person.image.url;
+        };
+    } else {
+      gapi.hangout.av.setMicrophoneMute(false);
+    };
+  } else {
+    gapi.hangout.av.setMicrophoneMute(true);
+  }
 }
 
 function configureSession() {
